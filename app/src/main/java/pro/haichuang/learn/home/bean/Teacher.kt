64be24e8865cd568cd9214d1751ec0b1bca3dcc8
jacy <jacyayj@qq.com>
2019-03-25 20:29:17 +0800

    补充提交

---
+++ app/src/main/java/pro/haichuang/learn/home/bean/Teacher.kt
@@ -0,0 +1,13 @@
+package pro.haichuang.learn.home.bean
+
+import android.databinding.BaseObservable
+
+class Teacher : BaseObservable() {
+
+    var teacherImg = ""
+    var teachername = ""
+    var skill = ""
+    var subject = 1
+    var type = ""
+
+}
